2b218a3a89609d0f0109eca2f2d9f38d5e8d6702
en-jn <en_jn@naver.com>
2021-10-13 16:38:58 +0900

    [baekjoon] 2557 Hello World

---
+++ Java/단계별로_풀어보기/01_입출력과_사칙연산/src/B2557.java
@@ -0,0 +1,13 @@
+/**
+ * BOJ 2557 : Hello World
+ * 
+ * @author en-jn
+ * {@link https://github.com/en-jn}
+ *
+ */
+
+public class B2557 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
