ee567872b353824a26a31c9a437320f7ab9caa64
en-jn <en_jn@naver.com>
2021-10-15 22:19:23 +0900

    [baekjoon] 10718 We love kriii

---
+++ Java/단계별로_풀어보기/01_입출력과_사칙연산/src/B10718.java
@@ -0,0 +1,13 @@
+/**
+ * BOJ 10718 : We love kriii
+ * 
+ * @author en-jn
+ * {@link https://github.com/en-jn}
+ *
+ */
+
+public class B10718 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군\n강한친구 대한육군");
+	}
+}
